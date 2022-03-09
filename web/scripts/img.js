4c30b0d3714258fa86061198f62edb74650e8964
Juan Pablo <juanpc.559@gmail.com>
2022-03-09 05:11:38 -0600

    login empezado

---
+++ web/scripts/img.js
@@ -0,0 +1,10 @@
+const url = "/web/img";
+
+function getIcono(n = int) {
+    switch(n){
+        case 1: return url + "/facebook.png"
+        case 2:
+        case 3:
+            case 4:
+    }
+}
---
+++ web/scripts/vistas.js
@@ -0,0 +1,15 @@
+function getAcademia(){
+    setTimeout(() => {
+        window.location.href="/web/html/academia.html";
+    },2000);
+}
+function getConocenos() {    
+    setTimeout(() => {
+    window.location.href="/web/html/conocenos.html";
+    },2000);
+}
+function getRegistro() {
+    setTimeout(() => {
+    window.location.href="/web/html/login.html";
+    },2000);
+}
